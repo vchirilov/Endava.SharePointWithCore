ff44c0768826f8a079437956a9e45d0b418fc41d
Veaceslav Chirilov <veaceslav.chirilov@endava.com>
2020-05-12 21:25:58 +0300

    Add project files.

---
+++ SharePointWithCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharePointWithCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
